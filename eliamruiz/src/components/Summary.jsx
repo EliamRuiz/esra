28fe601b1283263671acb5f71ea68942ebf865e6
eliam.ruiz <eliam.ruiz@upr.edu>
2025-02-13 10:15:48 -0400

    Update resume file again

--- eliamruiz/src/components/Summary.jsx
+++ eliamruiz/src/components/Summary.jsx
@@ -72,7 +72,7 @@ export default function Summary() {
             </div>
             <div className="flex mt-6 justify-center md:justify-center lg:justify-start">
               <a
-                href="./CV_SWE.pdf"
+                href="./EliamSweCv.pdfs.pdf"
                 download="EliamRuiz_CV.pdf"
                 className="px-4 py-2 bg-teal-600 text-white text-lg font-semibold rounded-lg shadow-lg hover:bg-teal-700 transition duration-300"
               >
